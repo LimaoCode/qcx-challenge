374dc97eb47060aa4fe02afcf34a2e6842b7a3ef
João Vitor <contatolimaocode@gmail.com>
2023-08-02 18:12:17 -0300

    feat: add header and footer layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+import Image from "next/image";
 import "./globals.css";
+import styles from "./layout.module.css";
+import logo from "@/assets/code.png";
 import { Roboto } from "next/font/google";
+import Link from "next/link";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -14,7 +18,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <header className={styles.header}>
+          <nav className={styles.nav}>
+            <Image src={logo} width={128} height={32} alt="logo pessoal" />
+            <ul className={styles.menuList}>
+              <Link href="#home-section" className={styles.menuListItem}>
+                Home
+              </Link>
+              <Link href="#about-section" className={styles.menuListItem}>
+                Sobre
+              </Link>
+              <Link href="#challenge-section" className={styles.menuListItem}>
+                Desafio
+              </Link>
+            </ul>
+          </nav>
+        </header>
+        {children}
+        <footer className={styles.footer}>
+          <h5 className={styles.footerContent}>
+            Desafio Desenvolvedor Front Qconcursos.com
+          </h5>
+        </footer>
+      </body>
     </html>
   );
 }
