4586c45caeac07fa68f2374cfb90b2ebc3940b8c
João Vitor <contatolimaocode@gmail.com>
2023-08-02 21:21:13 -0300

    fix: change usercard component name

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,7 +3,9 @@ import styles from "./userCard.module.css";
 export default function UserCard() {
   return (
     <div className={styles.card}>
-      <div></div>
+      <div>
+        <h1>teste</h1>
+      </div>
     </div>
   );
 }
