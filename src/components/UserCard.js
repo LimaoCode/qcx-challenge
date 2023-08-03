183c1a09cd3665b6e2d76a0969e023cb5a7f60cb
João Vitor <contatolimaocode@gmail.com>
2023-08-03 13:25:50 -0300

    feat: add user card style

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,10 +1,36 @@
+import Link from "next/link";
 import styles from "./userCard.module.css";
+import Image from "next/image";
+import logo from "@/assets/code.png";
 
 export default function UserCard() {
   return (
     <div className={styles.card}>
-      <div>
-        <h1>teste</h1>
+      <div className={styles.avatarAside}>
+        <Image src={logo} alt="logo" className={styles.avatar} />
+        <Link href="#">
+          <span>Visitar Perfil</span>
+        </Link>
+      </div>
+      <div className={styles.dataAside}>
+        <div className={styles.data}>
+          <div className={styles.dataItem}>
+            <h4 className={styles.dataText}>Repositórios:</h4>
+            <span className={styles.dataValue}>15</span>
+          </div>
+          <div className={styles.dataItem}>
+            <h4 className={styles.dataText}>Seguidores:</h4>
+            <span className={styles.dataValue}>15</span>
+          </div>
+          <div className={styles.dataItem}>
+            <h4 className={styles.dataText}>Seguindo:</h4>
+            <span className={styles.dataValue}>15</span>
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.repositoryButton}>Ver repositórios</button>
+          <button className={styles.favoritesButton}>Ver favoritos</button>
+        </div>
       </div>
     </div>
   );
